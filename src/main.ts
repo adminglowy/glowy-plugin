cca0c7e43837113efa919c587463a58ed2f3ea74
Rodrigo Waltenberg <rodrigo@glowy.com.br>
2019-07-31 19:40:53 -0300

    Assina os eventos do frame

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import doFrameAction from './actions/frame-actions'
+import getScrollableParent from './utils/get-scrollable-parent'
 import registerFrames from './actions/register'
 
 (function glowyInstall () {
@@ -7,6 +9,36 @@ import registerFrames from './actions/register'
     }
     return
   }
+
+  window.addEventListener('message', (event) => {
+    if (!event.data || event.data.sender !== 'Glowy') {
+      return
+    }
+
+    const data = event.data as GlowyMessage
+
+    if (data.version !== '1') {
+      console.error(`This library does not support Glowy Message v${data.version}. Supported: v1`)
+      return
+    }
+
+    const glowy = window.glowy!
+
+    const glowyFrame = glowy.frames.find(m => m.id === data.frame)
+
+    if (!glowyFrame) {
+      return
+    }
+
+    const frame = glowyFrame.frame
+    const scrollContainer = getScrollableParent(frame)
+
+    doFrameAction({
+      frame,
+      scrollContainer,
+      scrollOffset: 0
+    }, data.action, data.payload)
+  })
 
   const glowy: GlowyModule = {
     version: 'v1.0',
--- src/module.d.ts
+++ src/module.d.ts
@@ -1,5 +1,12 @@
 declare var WHITELABEL_ENDPOINT: string
 
+interface GlowyMessage {
+  frame: string
+  sender: 'Glowy'
+  version: string
+  action: string
+  payload: any
+}
 declare interface GlowyFrame {
   id: string
   frame: HTMLIFrameElement
